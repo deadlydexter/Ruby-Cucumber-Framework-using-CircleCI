c591f2ab0605a9809cfa1bd091db9620f41a8217
satheesh swaminathan <32887710+deadlydexter@users.noreply.github.com>
2020-06-05 23:45:53 -0400

    test10

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -1,11 +1,11 @@
 Given(/^I have no cheese$/) do
-  puts "I have no cheese"
+  log("I have no cheese")
 end
 
 When(/^I press the make cheese button$/) do
-  puts "I press the make cheese button"
+  log "I press the make cheese button"
 end
 
 Then(/^I should have (\d+) piece of cheese$/) do |arg|
-  puts "I should have 1 piece of cheese"
+  log "I should have 1 piece of cheese"
 end

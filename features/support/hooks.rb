f8dd6102e414bd4b7cc85dfe310cc33918d041ab
satheesh swaminathan <32887710+deadlydexter@users.noreply.github.com>
2020-06-09 19:21:07 -0400

    added chromedriver to path

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,12 +2,13 @@
 
 Before('@AdoptingAPuppy') do |scenario|
   log("Initializing Chrome Browser: ")
+  Selenium::WebDriver::Chrome.driver_path = "features/support/driver/chromedriver.exe"
   @browser = Watir::Browser.new :chrome
   @browser.window.maximize
 end
 
 
 After('@AdoptingAPuppy') do
-  log("Close Chrome Browser Instance")
+  log("Closing Chrome Browser Instance")
   @browser.close
 end

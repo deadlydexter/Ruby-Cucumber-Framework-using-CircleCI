761a192a804fe3f0cb5002eabb8d34a50ba4d9d9
satheesh swaminathan <32887710+deadlydexter@users.noreply.github.com>
2020-06-09 20:08:47 -0400

    Fix

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -3,7 +3,7 @@
 Before('@AdoptingAPuppy') do |scenario|
   log("Initializing Chrome Browser: ")
   #Selenium::WebDriver::Chrome::Service.driver_path = "features/support/driver/chromedriver.exe"
-  @browser = Watir::Browser.new :chrome
+  @browser = Watir::Browser.new :chrome, options: {args: "--disable-dev-shm-usage"}
   @browser.window.maximize
 end
 
